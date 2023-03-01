e2d671ee07abce780f0c205c7571ce00542cfd83
Delbot19 <delbotoussou00@gmail.com>
2023-03-01 18:11:06 +0000

    ajout du header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>Projet avec delbot</div>
-      </header>
+      <Header></Header>
     </div>
   );
 }
