dd5843bb757f3396dc4b45a7d1e906e0b53310a7
Delbot19 <delbotoussou00@gmail.com>
2023-03-03 18:52:34 +0000

    plantItem

--- src/Datas/PlantLists.js
+++ src/Datas/PlantLists.js
@@ -3,8 +3,6 @@ export const plantList = [
         name: 'monstera',
         category: 'classique',
         id: '1ed',
-        isBestSale: true,
-        isSpecialOffer: true,
         water: 2,
         light: 1
     },
@@ -12,8 +10,6 @@ export const plantList = [
         name: 'ficus lyrata',
         category: 'classique',
         id: '2ab',
-        isBestSale: true,
-        isSpecialOffer: false,
         water: 3,
         light: 2
     },
@@ -21,8 +17,6 @@ export const plantList = [
         name: 'pothos argenté',
         category: 'classique',
         id: '3sd',
-        isBestSale: false,
-        isSpecialOffer: true,
         water: 2,
         light: 1
     },
@@ -30,8 +24,6 @@ export const plantList = [
         name: 'yucca',
         category: 'classique',
         id: '4kk',
-        isBestSale: true,
-        isSpecialOffer: false,
         water: 3,
         light: 1
     },
@@ -39,8 +31,6 @@ export const plantList = [
         name: 'olivier',
         category: 'extérieur',
         id: '5pl',
-        isBestSale: false,
-        isSpecialOffer: true,
         water: 2,
         light: 2
     },
@@ -48,8 +38,6 @@ export const plantList = [
         name: 'géranium',
         category: 'extérieur',
         id: '6uo',
-        isBestSale: false,
-        isSpecialOffer: false,
         water: 3,
         light: 1
     },
@@ -57,8 +45,6 @@ export const plantList = [
         name: 'basilique',
         category: 'extérieur',
         id: '7ie',
-        isBestSale: false,
-        isSpecialOffer: false,
         water: 2,
         light: 1
     },
@@ -66,8 +52,6 @@ export const plantList = [
         name: 'aloe',
         category: 'plante grasse',
         id: '8fp',
-        isBestSale: false,
-        isSpecialOffer: true,
         water: 2,
         light: 2
     },
@@ -75,8 +59,6 @@ export const plantList = [
         name: 'succulente',
         category: 'plante grasse',
         id: '9vn',
-        isBestSale: true,
-        isSpecialOffer: false,
         water: 2,
         light: 3
     }
