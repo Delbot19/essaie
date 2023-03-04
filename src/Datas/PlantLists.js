7189c883d6b077d623acf26d7adb03e6c5a05a3d
Delbot19 <delbotoussou00@gmail.com>
2023-03-04 14:17:00 +0000

    ajout de footer et formulaire

--- src/Datas/PlantLists.js
+++ src/Datas/PlantLists.js
@@ -1,65 +1,76 @@
+import monstera from '../assets/monstera.jpg'
+
 export const plantList = [
-    {
-        name: 'monstera',
-        category: 'classique',
-        id: '1ed',
-        water: 2,
-        light: 1
-    },
-    {
-        name: 'ficus lyrata',
-        category: 'classique',
-        id: '2ab',
-        water: 3,
-        light: 2
-    },
-    {
-        name: 'pothos argenté',
-        category: 'classique',
-        id: '3sd',
-        water: 2,
-        light: 1
-    },
-    {
-        name: 'yucca',
-        category: 'classique',
-        id: '4kk',
-        water: 3,
-        light: 1
-    },
-    {
-        name: 'olivier',
-        category: 'extérieur',
-        id: '5pl',
-        water: 2,
-        light: 2
-    },
-    {
-        name: 'géranium',
-        category: 'extérieur',
-        id: '6uo',
-        water: 3,
-        light: 1
-    },
-    {
-        name: 'basilique',
-        category: 'extérieur',
-        id: '7ie',
-        water: 2,
-        light: 1
-    },
-    {
-        name: 'aloe',
-        category: 'plante grasse',
-        id: '8fp',
-        water: 2,
-        light: 2
-    },
-    {
-        name: 'succulente',
-        category: 'plante grasse',
-        id: '9vn',
-        water: 2,
-        light: 3
-    }
+	{
+		name: 'monstera',
+		category: 'classique',
+		id: '1ed',
+		light: 2,
+		water: 3,
+		cover: monstera
+	},
+	{
+		name: 'ficus lyrata',
+		category: 'classique',
+		id: '2ab',
+		light: 3,
+		water: 1,
+		cover: monstera
+	},
+	{
+		name: 'pothos argenté',
+		category: 'classique',
+		id: '3sd',
+		light: 1,
+		water: 2,
+		cover: monstera
+	},
+	{
+		name: 'yucca',
+		category: 'classique',
+		id: '4kk',
+		light: 3,
+		water: 1,
+		cover: monstera
+	},
+	{
+		name: 'olivier',
+		category: 'extérieur',
+		id: '5pl',
+		light: 3,
+		water: 1,
+		cover: monstera
+	},
+	{
+		name: 'géranium',
+		category: 'extérieur',
+		id: '6uo',
+		light: 2,
+		water: 2,
+		cover: monstera
+	},
+	{
+		name: 'basilique',
+		category: 'extérieur',
+		id: '7ie',
+		light: 2,
+		water: 3,
+		cover: monstera
+	},
+	{
+		name: 'aloe',
+		category: 'plante grasse',
+		id: '8fp',
+		light: 2,
+		water: 1,
+		cover: monstera
+	},
+	{
+		name: 'succulente',
+		category: 'plante grasse',
+		id: '9vn',
+		light: 2,
+		water: 1,
+		cover: monstera
+	}
 ]
--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,16 +1,18 @@
 import React from 'react'
 
 function CareScale({scaleValue,careType}) {
-
-
     const range = [1, 2, 3]
-
     const emoji= (careType==='light')? '☀️':'💧'
-
+    function handleClick() {
+        (scaleValue===1)?alert(`Cette plante requiert peu ${careType==='light'?'de lumiere':'d\'arrosage'}`):
+        (scaleValue===2)?alert(`Cette plante requiert modérement ${careType==='light'?'de lumiere':'d\'arrosage'}`):
+        alert(`Cette plante requiert beaucoup ${careType==='light'?'de lumiere':'d\'arrosage'}`)
+    }
     return (
-        <div>
+        <div  onClick={() => handleClick()}>
             {range.map((rangeElem) =>
                 scaleValue >= rangeElem && <span key={rangeElem.toString()}>{emoji}</span>
+                //{emoji.repeat(scaleValue)}
             )}
         </div>
     )
